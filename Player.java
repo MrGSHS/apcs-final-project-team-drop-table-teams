24d88545b125897119fac4d1fa707b2fdd2bdd23
John Armgardt 2018 <jarmgar8@students.d125.org>
2016-04-26 15:58:07 -0500

    class construction kind of

---
+++ Player.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Player1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private Hand h1, h2;
+    public Player()
+    {
+        h1 = new Hand();
+        h2 = new Hand();
+    }
+}
