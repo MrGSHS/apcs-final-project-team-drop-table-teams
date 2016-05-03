56315b85cb58def6dfaf25d88f1c6e78ae7674c4
John Armgardt 2018 <jarmgar8@students.d125.org>
2016-05-03 15:23:42 -0500

    jframe attempt failed

--- TTT.java
+++ TTT.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 /**
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class TTT //Tic Tac Toe
+public class TTT extends JFrame//Tic Tac Toe
 {
     private int[][] board;
     public TTT()
@@ -22,23 +22,23 @@ public class TTT //Tic Tac Toe
         board = new int[r][c];
         display();
     }
-    
-    public static void display()
-    {
-        JFrame f = new JFrame("Tic Tac Toe");
-        f.setSize(500,500);
-        f.setVisible(true);
+
+    public void display() {
+        JFrame frame = new JFrame("Tic Tac Toe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(500,500));
+        frame.pack();
+        frame.setVisible(true);
     }
-    
-    public void paint(Graphics g)
-    {
-        JFrame f = new JFrame("Tic Tac Toe");
-        f.setSize(500,500);
-        f.setVisible(true);
-        for (int r = 0; r < board.length; r++){
-            for (int c = 0; c < board[0].length; c++){
-                g.drawRect(500*r,500*c,500/board.length,500/board[0].length);
-            }
-        }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+//         for (int r = 0; r < board.length; r++){
+//             for (int c = 0; c < board[0].length; c++){
+//                 g.drawRect(500*r,500*c,500/board.length,500/board[0].length);
+//             }
+//         }
+g.setColor(Color.black);
+g.drawRect(0,0,100,100);
     }
 }
