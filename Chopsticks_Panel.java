f9e5b149a8f50c3cc953b75b312a592b0a0fd20f
SBlue3 <sbhaska8@students.d125.org>
2016-05-09 14:40:18 -0500

    Chopsticks Logic

--- Chopsticks_Panel.java
+++ Chopsticks_Panel.java
@@ -18,7 +18,11 @@ public class Chopsticks_Panel extends JPanel implements MouseListener, ActionLis
     int mouseX, mouseY;
     int mouseC = 0;
     boolean win = false;
-    int choice1, choice2;
+    int choice1 = 0;
+    int p1 = 1;
+    int p2 = 1;
+    int p3 = 1;
+    int p4 = 1;
     
     public Chopsticks_Panel() {
         addMouseListener(this);
@@ -29,23 +33,30 @@ public class Chopsticks_Panel extends JPanel implements MouseListener, ActionLis
         int w = getWidth();
         int h = getHeight();
         setBackground(Color.lightGray);
-        g.setColor(Color.blue);
         
+        g.setColor(Color.blue);
+        g.drawString("Player 1", 210, 10);
+        g.setColor(Color.red);
+        g.drawString("Player 2", 210, 470);
+        
+        g.setColor(Color.black);
         if( mouseC % 4 == 0 ){
-          g.drawString("Player 1 choose your hand", 150, 50);
+          g.drawString("Player 1 choose your hand", 150, 230);
         }
         else if( mouseC % 4 == 1 ){
-          g.drawString("Player 1 choose opponent's hand", 150, 50);
+          g.drawString("Player 1 choose opponent's hand", 150, 230);
         }
         else if( mouseC % 4 == 2 ){
-          g.drawString("Player 2 choose your hand", 150, 50);
+          g.drawString("Player 2 choose your hand", 150, 230);
         }
         else if( mouseC % 4 == 3 ){
-          g.drawString("Player 2 choose oppenent's hand", 150, 50);
+          g.drawString("Player 2 choose oppenent's hand", 150, 230);
         }
         
-        
-        
+        g.drawString(""+p1, 120, 370);
+        g.drawString(""+p2, 370, 370);
+        g.drawString(""+p3, 120, 120);
+        g.drawString(""+p4, 370, 120);
     }
 
     public void mouseClicked(MouseEvent me) {
@@ -56,16 +67,45 @@ public class Chopsticks_Panel extends JPanel implements MouseListener, ActionLis
         mouseX = me.getX();
         mouseY = me.getY();
         if( mouseC % 4 == 0 && mouseY > 250){
-           mouseC++;
+           if (mouseX < 250 && p1 != 0){
+               choice1 = p1;
+               mouseC++;
+           }
+           else if (mouseX > 250 && p2 != 0){
+               choice1 = p2;
+               mouseC++;
+           }
+           
         }
         else if( mouseC % 4 == 1 && mouseY < 250){
-          mouseC++;
+           if (mouseX < 250 && p3 != 0){
+               p3 = (p3 + choice1)%5;
+               mouseC++;
+           }
+           else if (mouseX > 250 && p4 != 0){
+               p4 = (p4 + choice1)%5;
+               mouseC++;
+           }
         }
         else if( mouseC % 4 == 2 && mouseY < 250){
-          mouseC++;
+           if (mouseX < 250 && p3 != 0){
+               choice1 = p3;
+               mouseC++;
+           }
+           else if (mouseX > 250 && p4 != 0){
+               choice1 = p4;
+               mouseC++;
+           }
         }
         else if( mouseC % 4 == 3 && mouseY > 250){
-          mouseC++;
+           if (mouseX < 250 && p1 != 0){
+               p1 = (p1 + choice1)%5;
+               mouseC++;
+           }
+           else if (mouseX > 250 && p2 != 0){
+               p2 = (p2 + choice1)%5;
+               mouseC++;
+           }
         }
         //mouseC++;
         repaint();
@@ -84,6 +124,6 @@ public class Chopsticks_Panel extends JPanel implements MouseListener, ActionLis
     }
     
     public void actionPerformed(ActionEvent ae) {
-        repaint();
+        
     }
 }
