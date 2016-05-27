597eeeca540189d13f34c76c8ae1fa5e3122a656
Danny Attia <dattia8@students.d125.org>
2016-05-26 23:25:40 -0500

    Finished RPS animations and finishing touches

--- RPS.java
+++ RPS.java
@@ -8,29 +8,55 @@ import java.io.*;
 
 /**
  * Write a description of class TTT_Frame here.
- * 
+ *
  * @author (your name)
  * @version (a version number or a date)
  */
 
-public class RPS extends JPanel implements MouseListener, MouseMotionListener{
+public class RPS extends JPanel implements MouseListener, MouseMotionListener, ActionListener{
 
     /**
-     * 
+     *
      */
-    private static final long serialVersionUID = 1L;
-    int mouseX, mouseY;
+    
+    //coordinates of the user rock, paper, and scissors (left side)
+    private double userRockX = 50;
+    private double userRockY = 50;
+    private double userPaperX = 50;
+    private double userPaperY = 175;
+    private double userScissorsX = 50;
+    private double userScissorsY = 300;
 
+    //coordinates of the computer rock, paper, and scissors (right side)
+    private double compRockX = 375;
+    private double compRockY = 50;
+    private double compPaperX = 375;
+    private double compPaperY = 175;
+    private double compScissorsX = 375;
+    private double compScissorsY = 300;
+
+    //booleans for hover highlight effect
     private boolean isHoveringOverRock = false;
     private boolean isHoveringOverPaper = false;
     private boolean isHoveringOverScissors = false;
-    private boolean choiceMade = false;
-    private int userChoice;
-    private int compChoice;
+    
+    //choice variables 
+    private int userChoice = 0;
+    private int compChoice = 0;
+    
+    //scores
     private int userScore = 0;
     private int compScore = 0;
+    
+    //result (win, loss, or draw) in string form and in int form
     private String result;
     private int iresult = 0;
+    
+    //progression booleans
+    private boolean choiceMade = false;
+    private boolean scoreUpdated = false;
+    private boolean userAnimationFinished = false;
+    private boolean compAnimationFinished = false;
 
     //images
     BufferedImage userRock= null;
@@ -42,11 +68,16 @@ public class RPS extends JPanel implements MouseListener, MouseMotionListener{
     BufferedImage compRock = null;
     BufferedImage compPaper = null;
     BufferedImage compScissors = null;
+    
 
     public RPS() {
+
+        //listeners
         addMouseListener(this);
         addMouseMotionListener(this);
-        try 
+
+        //images
+        try
         {
             userRock = ImageIO.read(new File("Rock.png"));
             userRockHighlight = ImageIO.read(new File("highlightedRock.png"));
@@ -63,218 +94,290 @@ public class RPS extends JPanel implements MouseListener, MouseMotionListener{
             System.out.println("Image could not be read");
             System.exit(1);
         }
+        
+        //timer for animations
+        Timer timer = new Timer(5, this);
+        timer.start();
 
     }
 
     public void paintComponent(Graphics g) {
-        int rockLocX;
-        int rockLocY;
-        int paperLocX;
-        int paperLocY;
-        int scissorsLocX;
-        int scissorsLocy;
         super.paintComponent(g);
-        int w = getWidth();
-        int h = getHeight();
         setBackground(new Color(0,255,225));
-        /*
-        BufferedImage userRock= null;
-        BufferedImage userRockHighlight = null;
-        BufferedImage userPaper=null;
-        BufferedImage userPaperHighlight = null;
-        BufferedImage userScissors= null;
-        BufferedImage userScissorsHighlight = null;
-        BufferedImage compRock = null;
-        BufferedImage compPaper = null;
-        BufferedImage compScissors = null;
-         */
-
-
- 
-        if (!choiceMade)
-        {
+     
+        //draw user rock, paper, and scissors (left side) and highlight effect
+        if(userChoice !=1){
             if (!isHoveringOverRock)
-                g.drawImage(userRock,50,50,null); //userRock
+                g.drawImage(userRock,50,50, null); //userRock
             else
                 g.drawImage(userRockHighlight,50,50,null);
+        }
+        if(userChoice != 2) {
             if (!isHoveringOverPaper)
-                g.drawImage(userPaper,50,175,null); 
+                g.drawImage(userPaper,50,175,null);
             else
                 g.drawImage(userPaperHighlight,50,175,null);
+        }
+        if(userChoice !=3) {
             if (!isHoveringOverScissors)
                 g.drawImage(userScissors,50,300,null); //userScissors
             else
                 g.drawImage(userScissorsHighlight,50,300,null);
-
+        }
+        
+        //draw computer rock, paper, and scissors
+        if (compChoice !=1)
+        {
             g.drawImage(compRock,375,50,null); //compRock
+        }
+        if (compChoice !=2)
+        {
             g.drawImage(compPaper,375,175,null); //compPaper
+        }
+        if (compChoice !=3)
+        {
             g.drawImage(compScissors,375,300,null); //compScissors
         }
-        else
-        {
-            choiceLogic(g);
-        }
-        g.setColor(Color.black);
-        g.drawString("Player 1", 10, 20);
-        g.drawString("Computer", 420, 20);
-        g.drawString(""+userScore, 30, 40);
-        g.drawString(""+compScore, 450, 40);
 
+        //animations for user rock, paper, and scissors
+        if(choiceMade) {
+            if (userChoice == 1)
+            {
+                g.drawImage(userRock,(int)userRockX,(int)userRockY,null);
+                if (userRockX < 212 && userRockY < 105)
+                {
+                    userRockX+=9;
+                    userRockY+=3;
+                }
+
+                else
+                {
+                    userAnimationFinished = true;
+                }
+
+            }
+            if (userChoice == 2)
+            {
+                g.drawImage(userPaper,(int)userPaperX,(int)userPaperY,null);
+                if (userPaperX < 212 && userPaperY > 105)
+                {
+                    userPaperX+=9;
+                    userPaperY-=3.86;
+                }
+
+                else
+                {
+                    userAnimationFinished = true;
+                }
+            }
+            if (userChoice == 3)
+            {
+                g.drawImage(userScissors,(int)userScissorsX,(int)userScissorsY,null);
+                if (userScissorsX < 212 && userPaperY > 105)
+                {
+                    userScissorsX+=9;
+                    userScissorsY-=10.7;
+                }
+                else
+                {
+                    userAnimationFinished = true;
+                }
+            }
+            
+            //animations for computer rock, paper, and scissors
+            if (compChoice == 1)
+            {
+                g.drawImage(compRock, (int)compRockX, (int)compRockY, null);
+                if (compRockX > 212 && compRockY < 247)
+                {
+                    compRockX-=9;
+                    compRockY+=11.04;
+                }
+                else
+                {
+                    compAnimationFinished = true;
+                }
+            }
+            if (compChoice == 2)
+            {
+                g.drawImage(userPaper, (int)compPaperX, (int)compPaperY, null);
+                if (compPaperX > 212 && compPaperY < 249)
+                {
+                    compPaperX-=9;
+                    compPaperY+=4.15;
+                }
+                else
+                {
+                    compAnimationFinished = true;
+                }
+            }
+            if (compChoice == 3)
+            {
+                g.drawImage(userScissors, (int)compScissorsX, (int)compScissorsY, null);
+                if (compScissorsX > 212 && compScissorsY > 252)
+                {
+                    compScissorsX-=9;
+                    compScissorsY-=2.76;
+                }
+                else
+                {
+                    compAnimationFinished = true;
+                }
+            }
+
+            //determine outcome
+            if (userChoice == compChoice)
+            {
+                result = "DRAW";
+                iresult = 1;
+            }
+            if (userChoice == 1 && compChoice == 2)
+            {
+                result = "LOSE";
+                iresult = 3;
+            }
+            if (userChoice == 1 && compChoice == 3)
+            {
+                result = "WIN";
+                iresult = 2;
+            }
+            if (userChoice == 2 && compChoice == 1)
+            {
+                result = "WIN";
+                iresult = 2;
+            }
+            if (userChoice == 2 && compChoice == 3)
+            {
+                result = "LOSE";
+                iresult = 3;
+            }
+            if (userChoice == 3 && compChoice == 1)
+            {
+                result = "LOSE";
+                iresult = 3;
+            }
+            if (userChoice == 3 && compChoice == 2)
+            {
+                result = "WIN";
+                iresult = 2;
+            }
+
+            //post animations
+            if (userAnimationFinished && compAnimationFinished)
+            {
+                //back button
+                g.setColor(Color.lightGray);
+                g.fillRect(200,400,100,100);
+                g.setColor(Color.red);
+                g.drawRect(200,400,100,100);
+                g.setColor(Color.black);
+                g.drawString("Back",235,440);
+                
+                //text that displays "WIN", "LOSS", or "DRAW"
+                g.drawString(result,230,20);
+                
+                //update score
+                if (!scoreUpdated)
+                {
+                    if (iresult == 1)
+                    {
+                    }
+                    else if(iresult == 2){
+                        userScore++;
+                    }
+                    else if (iresult == 3){
+                        compScore++;
+                    }
+                    scoreUpdated = true;
+                }
+            }
+        }
+        
+        //menu button
+        g.setColor(Color.lightGray);
+        g.fillRect(400,425,100,100);
+        g.setColor(Color.red);
+        g.drawRect(400,425,100,100);
+        g.setColor(Color.black);
+        g.drawString("Menu", 430, 450);
+        
+        
+        
+        g.setColor(Color.black);
+        
+        //user's score in upper left
+        g.drawString("Player 1", 65, 20);
+        g.drawString(""+userScore, 85, 40);
+        
+        //computer's score in upper right
+        g.drawString("Computer", 385, 20);
+        g.drawString(""+compScore, 412, 40);
+        
+        
     }
 
-    public void choiceLogic(Graphics g)
+   
+    public void actionPerformed(ActionEvent ae)
     {
-        compChoice= (int)((Math.random())*3+1);
-        if (userChoice == compChoice)
-        { 
-            result = "DRAW";
-            iresult = 1;
-        }
-        if (userChoice == 1 && compChoice == 2)
-        {
-            result = "LOSE";
-            iresult = 3;
-        }
-        if (userChoice == 1 && compChoice == 3)
-        {
-            result = "WIN";
-            iresult = 2;
-        }
-        if (userChoice == 2 && compChoice == 1)
-        {
-            result = "WIN";
-            iresult = 2;
-        }
-        if (userChoice == 2 && compChoice == 3)
-        {
-            result = "LOSE";
-            iresult = 3;
-        }
-        if (userChoice == 3 && compChoice == 1)
-        {
-            result = "LOSE";
-            iresult = 3;
-        }
-        if (userChoice == 3 && compChoice == 2)
-        {
-            result = "WIN";
-            iresult = 2;
-        }
-
-        //Second Part
-        if (userChoice == 1)
-        {
-            g.drawImage(userRock, 213 ,105, null);
-        }
-        if (userChoice == 2)
-        {
-            g.drawImage(userPaper, 213 ,105, null);
-        }
-        if (userChoice == 3)
-        {
-            g.drawImage(userScissors, 213 ,105, null);
-        }
-        if (compChoice == 1)
-        {
-            g.drawImage(userRock, 213, 250, null);
-        }
-        if (compChoice == 2)
-        {
-            g.drawImage(userPaper, 213, 250, null);
-        }
-        if (compChoice == 3)
-        {
-            g.drawImage(userScissors, 213, 250, null);
-        }
-
-        if (iresult == 1){
-            g.drawString(result,235,20);
-        }
-        else if(iresult == 2){
-            g.drawString(result,235,20);
-            userScore++;
-        }
-        else if (iresult == 3){
-            g.drawString(result,235, 20);
-            compScore++;
-        }
-
-        if ( userScore == 2){
-            g.drawString("YOU WIN",225, 420);
-            g.setColor(Color.lightGray);
-            g.fillRect(400,400,100,100);
-
-            g.setColor(Color.red);
-            g.drawLine(400,400,400,500);
-            g.drawLine(400,400,500,400);
-            g.drawString("Menu",430,440);
-        }
-        else if( compScore == 2){
-            g.drawString("YOU LOSE",225, 420);
-            g.setColor(Color.lightGray);
-            g.fillRect(400,400,100,100);
-
-            g.setColor(Color.red);
-            g.drawLine(400,400,400,500);
-            g.drawLine(400,400,500,400);
-            g.drawString("Menu",430,440);
-        }
-        else{
-            g.setColor(Color.red);
-            g.setColor(Color.lightGray);
-            g.fillRect(200,400,100,100);
-            g.setColor(Color.red);
-            g.drawRect(200,400,100,100);
-            g.setColor(Color.black);
-            g.drawString("Back",235,440);
-            //choiceLogic(g);
-        }            
-
-        g.setColor(Color.black);
-        g.drawString("Player 1", 10, 20);
-        g.drawString("Computer", 420, 20);
-        g.drawString(""+userScore, 30, 40);
-        g.drawString(""+compScore, 450, 40);
-
-    }
-
-    public void highlightEffect()
-    {
-
+       //repaint() needs to be in actionPerformed so it will repaint correspondingly with the timer
+       repaint();
     }
 
     public void mouseClicked(MouseEvent me) {
-
     }
 
     public void mousePressed(MouseEvent me) {
         if (!choiceMade)
         {
+            //determining which choice the user clicked on 
             if ((me.getX() >= 50 && me.getX() <= 125) && (me.getY() >= 50 && me.getY() <= 157))
             {
                 userChoice = 1;
+                compChoice= (int)((Math.random())*3+1);
                 choiceMade = true;
+
             }
             if ((me.getX() >= 50 && me.getX() <= 125) && (me.getY() >= 175 && me.getY() <= 279))
             {
                 userChoice = 2;
+                compChoice= (int)((Math.random())*3+1);
                 choiceMade = true;
             }
             if ((me.getX() >= 50 && me.getX() <= 121) && (me.getY() >= 300 && me.getY() <= 405))
             {
                 userChoice = 3;
+                compChoice= (int)((Math.random())*3+1);
                 choiceMade = true;
             }
-            repaint();
         }
+        
+        //if back button clicked - a bunch of things get reset because game essentially restarts
         else if( me.getX() < 300 && me.getX() > 200 && me.getY() > 400){
             choiceMade = false;
-            repaint();
+            scoreUpdated = false;
+            userAnimationFinished = false;
+            compAnimationFinished = false;
+            userChoice = 0;
+            compChoice = 0;
+            userRockX = 50;
+            userRockY = 50;
+            userPaperX = 50;
+            userPaperY = 175;
+            userScissorsX = 50;
+            userScissorsY = 300;
+            compRockX = 375;
+            compRockY = 50;
+            compPaperX = 375;
+            compPaperY = 175;
+            compScissorsX = 375;
+            compScissorsY = 300;
         }
-        else if(me.getX() > 400 && me.getY() > 400){
-            String[]a = new String[0];
-            GameMenu.main(a);
+        
+        //menu button
+        if((me.getX() >= 400 && me.getX() <=500) && (me.getY() >= 400 && me.getY() <= 500)){
+            GameMenu.main(new String[0]);
         }
+
     }
 
     public void mouseReleased(MouseEvent me) {
@@ -287,15 +390,29 @@ public class RPS extends JPanel implements MouseListener, MouseMotionListener{
     }
 
     public void mouseMoved(MouseEvent me){
+        //assigns booleans that handle the hover effect
         if (!choiceMade){
             isHoveringOverRock = (me.getX() >= 50 && me.getX() <= 125) && (me.getY() >= 50 && me.getY() <= 157);
             isHoveringOverPaper = (me.getX() >= 50 && me.getX() <= 125) && (me.getY() >= 175 && me.getY() <= 279);
             isHoveringOverScissors  = (me.getX() >= 50 && me.getX() <= 121) && (me.getY() >= 300 && me.getY() <= 405);
-            repaint();
+
         }
     }
 
     public void mouseDragged(MouseEvent me){
+    }
+
+    //main method - makes it able to be run without game menu
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        RPS rps = new RPS();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(500, 500));
+        frame.add(rps);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 
 }
